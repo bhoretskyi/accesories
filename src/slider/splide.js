a94b6050a1359500efe32822ec3d90c76f45a3cf
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-04-01 17:44:59 +0200

    add 2 sliders

--- src/slider/splide.js
+++ src/slider/splide.js
@@ -1,8 +1,27 @@
 import Splide from '@splidejs/splide';
 
-document.addEventListener('DOMContentLoaded', function () {
-    new Splide('.splide',  {
-        type   : 'loop',
-        padding: '20%',
-      }).mount();
-});  
+new Splide('#slider1', {
+  speed: 800,
+  arrows: false,
+  pagination: false,
+  rewind: true,
+
+  trimSpace: false,
+
+  fixedWidth: 275,
+  gap: 20,
+  focus: 'center',
+  perPage: 3,
+}).mount();
+
+new Splide('#slider2', {
+  perPage: 1,
+  perMove: 1,
+  start: 1,
+
+  speed: 500,
+  width: '100%',
+  rewind: true,
+  pagination: false,
+  // focus: 'center',
+}).mount();
