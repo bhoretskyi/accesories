1c22dfc2b1650690dee60c50b2539b77953ac5b6
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-04-06 13:09:00 +0000

    Deploying to gh-pages from @ bhoretskyi/accesories@639aaabce1fc69b5d6b2066d7c961bc219ac0d7f 🚀

---
+++ index.82f909c6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("modal"),n=document.querySelectorAll(".openModalBtn"),t=document.getElementsByClassName("close")[0];n.forEach((function(n){n.addEventListener("click",(function(){e.style.display="block"}))})),t.addEventListener("click",(function(){e.style.display="none"})),window.addEventListener("click",(function(n){n.target==e&&(e.style.display="none")}))}();
+//# sourceMappingURL=index.82f909c6.js.map
---
+++ index.cc0b951c.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("modal"),n=document.querySelectorAll(".openModalBtn"),t=document.getElementsByClassName("close")[0];n.forEach((function(n){n.addEventListener("click",(function(){e.style.display="block"}))})),t.addEventListener("click",(function(){e.style.display="none"})),window.addEventListener("click",(function(n){n.target==e&&(e.style.display="none")}));
+//# sourceMappingURL=index.cc0b951c.js.map
