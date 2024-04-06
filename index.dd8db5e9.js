dcf59b881714218d9f2fbd9d90350e2bcbb1c0c7
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-04-06 13:39:55 +0000

    Deploying to gh-pages from @ bhoretskyi/accesories@9004cbd178059607f67fa2a6113ec71ad834bb17 🚀

---
+++ index.dd8db5e9.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("modal"),n=document.querySelectorAll(".openModalImg"),t=document.getElementsByClassName("close")[0];n.forEach((function(n){n.addEventListener("click",(function(){e.style.display="block"}))})),t.addEventListener("click",(function(){e.style.display="none"})),window.addEventListener("click",(function(n){n.target==e&&(e.style.display="none")}))}();
+//# sourceMappingURL=index.dd8db5e9.js.map
---
+++ index.f119d440.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("modal"),n=document.querySelectorAll(".openModalImg"),t=document.getElementsByClassName("close")[0];n.forEach((function(n){n.addEventListener("click",(function(){e.style.display="block"}))})),t.addEventListener("click",(function(){e.style.display="none"})),window.addEventListener("click",(function(n){n.target==e&&(e.style.display="none")}));
+//# sourceMappingURL=index.f119d440.js.map
