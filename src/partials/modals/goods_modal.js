373bcf7858a24c3b938a47f43524c0ea721f34b4
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-04-22 20:12:30 +0200

    add footer on mobile modal

--- src/partials/modals/goods_modal.js
+++ src/partials/modals/goods_modal.js
@@ -5,11 +5,13 @@ let closeModalBtn = document.getElementsByClassName('close')[0];
 openModalBtns.forEach(function(btn) {
     btn.addEventListener('click', function() {
         modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
 
     });
 });
 closeModalBtn.addEventListener('click', function() {
     modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
 });
 
 window.addEventListener('click', function(event) {
